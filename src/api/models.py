0064a8195fb019ae0656334d26e57c27a39a5b3d
TheRhettThompson <rthompsoncareer@gmail.com>
2023-01-19 01:52:32 +0000

    added to models

--- src/api/models.py
+++ src/api/models.py
@@ -5,7 +5,18 @@ db = SQLAlchemy()
 #CREATE YOUR DATABASE TABLES HERE
 class User(db.Model):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True),
+    firstname = db.Column(db.String(120), primary_key=False, nullable=False),
+    lastname = db.Column(db.String(120), primary_key=False, nullable=False),
+    username = db.Column(db.String(120), primary_key=False, nullable=False),
+    password = db.Column(db.String(120), primary_key=False, nullable=False),
+    validationcode = db.Column(db.String(120), primary_key=False, nullable=False),
+    email = db.Column(db.String(120), primary_key=False, nullable=False),
+    comments = db.Column(db.String(250), primary_key=False, nullable=True),
+    datejoined = db.Column(db.Date, primary_key=False, nullable=True),
+    lastlogin = db.Column(db.Date, primary_key=False, nullable=True),
+    active = db.Column(db.Boolean, primary_key=False, nullable=True),
+
 
     def __repr__(self):
         return f'<User {self.id}>'
@@ -13,5 +24,6 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "first name" :
             # do not serialize the password, its a security breach
         }
